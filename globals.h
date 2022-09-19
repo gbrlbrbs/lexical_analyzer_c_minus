d8a36174e6566b7246f755b8b5f955ba7e3c9bd3
gbrlbrbs <gabrielbarbosamartinz_@outlook.com>
2022-09-19 17:03:39 -0300

    add types, flags and finish getToken function

--- globals.h
+++ globals.h
@@ -32,6 +32,71 @@ extern FILE* source;
 extern FILE* listing;
 extern FILE* code;
 
+typedef enum {
+    Stmnt, Exp
+} NodeKind;
 
+typedef enum {
+    If, While, Return, Assign
+} StmntKind;
+
+typedef enum {
+    Op, Const, Id
+} ExpKind;
+
+typedef enum {
+    Void, Int, Bool
+} ExpType;
+
+#define MAXCHILDREN 3
+
+typedef struct TreeNode {
+    struct TreeNode *child[MAXCHILDREN];
+    struct TreeNode *sibling;
+    int lineno;
+    NodeKind node_kind;
+    union {StmntKind stmnt; ExpKind exp;} kind;
+    union {
+        TokenType op;
+        int val;
+        char *name;
+    } attr;
+    ExpType type;
+} TreeNode;
+
+/**************************************************/
+/***********   Flags for tracing       ************/
+/**************************************************/
+
+/* echo_source = TRUE causes the source program to
+ * be echoed to the listing file with line numbers
+ * during parsing
+ */
+extern int echo_source;
+
+/* trace_scan = TRUE causes token information to be
+ * printed to the listing file as each token is
+ * recognized by the scanner
+ */
+extern int trace_scan;
+
+/* trace_parse = TRUE causes the syntax tree to be
+ * printed to the listing file in linearized form
+ * (using indents for children)
+ */
+extern int trace_parse;
+
+/* trace_analyze = TRUE causes symbol table inserts
+ * and lookups to be reported to the listing file
+ */
+extern int trace_analyze;
+
+/* trace_code = TRUE causes comments to be written
+ * to the TM code file as code is generated
+ */
+extern int trace_code;
+
+/* error = TRUE prevents further passes if an error occurs */
+extern int error; 
 
 #endif
